52f2fc4ee96613a600881254c0f86fc8b6877b8d
Ananya Tiwari <ananyatiwari560035@gmail.com>
2026-09-25 23:17:43 +0530

    Updated problem02

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,12 +1,38 @@
-//2. Write a C program to add two numbers.
-#include<stdio.h>
-int main(){
-    int a,b,sum;
-    printf("enter the first number:");
-    scanf("%d",&a);
-    printf("enter the second number:");
-    scanf("%d",&b);
-    sum=a+b;
-    printf("the sum is:%d+%d=%d",a,b,sum);
+//2. Write a C program to add two numbers using 4 functions.
+#include <stdio.h>
+// Welcome to the program!!
+void userinput(int *lm, int *nk); // using call by reference
+int add(int y, int z); // using call by value with parameters passed
+void display(int y, int z, int sum); // using call by value  with parameter passed
+
+int main()
+{
+    int y, z, sum;
+
+    userinput(&y, &z);
+
+    sum = add(y, z);
+
+    display(y, z, sum);
+
     return 0;
+}
+
+void userinput(int *lm, int *nk)
+{
+    printf("Please enter two numbers of your choice! ");
+    printf("\nenter the first number:");
+    scanf("%d",lm);
+    printf("\nenter the second number:");
+    scanf("%d",nk);
+}
+
+int add(int y, int z)
+{
+    return y + z;
+}
+
+void display(int y, int z, int sum)
+{
+    printf(" sum is : %d + %d = %d\n", y, z, sum);
 }
